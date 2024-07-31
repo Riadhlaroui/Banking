3cdc6dc44e5b6e4657f6778eb6071f2d046f3784
Riadh laroui <larouiriadh@gmail.com>
2024-07-31 11:16:26 -0800

    fin 5

--- components/Copy.tsx
+++ components/Copy.tsx
@@ -20,11 +20,11 @@ const Copy = ({ title }: { title: string }) => {
   return (
     <Button
       data-state="closed"
-      className="mt-3 flex max-w-[320px] gap-4 bg-black-2"
+      className="mt-3 flex max-w-[320px] gap-4"
       variant="secondary"
       onClick={copyToClipboard}
     >
-      <p className="line-clamp-1 w-full max-w-full text-xs font-medium text-black-2">
+      <p className="line-clamp-1 w-full max-w-full text-xs font-medium">
         {title}
       </p>
 
